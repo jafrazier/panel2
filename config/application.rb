85dc31b080e9574b548c0bb0b7d7471e4fcd0879
jfrazier <johnfrazier1222@gmail.com>
2018-09-24 13:33:20 -0400

    precompile2

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,13 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-config.assets.enabled = true
-config.serve_static_assets = true
+
 Bundler.require(*Rails.groups)
 module Panel2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.enabled = true
+    config.serve_static_assets = true
     config.load_defaults 5.2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
